1a0cf8fd4c008c1ac9fbe627b516cab0dcfe3595
Iain Foster <9925082@student.toiohomai.ac.nz>
2018-08-30 14:48:32 +1200

    what a failure

---
+++ Q1 Test 1/Q1 Test 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------Question One-------------");
+            Console.WriteLine("-----This program can tell you if your number is odd or even-----");
+            Console.Write("Please enter a Whole number between 1 and 10:");
+
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Q2 Test1/Q2 Test1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("-----Question Two-----");
+            Console.WriteLine("This program will compute the avarage of 10 numbers");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a whole number:");
+                i += int.Parse(Console.ReadLine());
+                Console.WriteLine("the avrage number is:");
+                Console.Write(i / 10);
+                Console.ReadKey();
+            }
+        }
+    }
+}
